ae47df7482d4da726eb6d356f869cd97c363068c
brock_ant <louis.peller@orange.fr>
2022-08-05 14:31:54 +0200

    first commit

---
+++ mtb_router/src/main/java/com/mtb/router/MtbRouterApplication.java
@@ -0,0 +1,11 @@
+package com.mtb.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtbRouterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MtbRouterApplication.class, args);
+    }
+}
